125e17e4081cbc424f51016a17190e337e34e519
tolyan <tolyan@sctbelpa>
2019-08-22 14:44:44 +0300

    Добавьте файлы проекта.

---
+++ nanoFramework.Hardware.Esp32.RMT.NeoPixel/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace nanoFramework.Hardware.Esp32.RMT.NeoPixel
+{
+	public class nanoFramework.Hardware.Esp32.RMT.NeoPixel
+    {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+}
